ca7688b42038c9de9fadd9a9750844d1126fb7af
Gurur593 <bjk064128@gmail.com>
2026-08-30 17:26:25 +0300

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+aapp.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.post('/api/chat', async (req, res) => {
     try {
